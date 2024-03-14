64ea98e28d65283f41b0dec739d33bff77bb26aa
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-14 19:04:54 +0300

    added cart page to app component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react';
 
+import { CartPage } from 'pages/CartPage';
+
 import { WithTheme } from './providers/WithTheme';
 import { WithUIKitConfig } from './providers/WithUIKitConfig';
 
 export const App: FC = () => {
   return (
     <WithTheme>
-      <WithUIKitConfig></WithUIKitConfig>
+      <WithUIKitConfig>
+        <CartPage />
+      </WithUIKitConfig>
     </WithTheme>
   );
 };
