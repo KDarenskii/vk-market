014b63a962d0589550896e469525d2b4abd3e6c2
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-14 19:00:27 +0300

    re-exported entities cart components

---
+++ src/entities/cart/index.ts
@@ -0,0 +1,4 @@
+export { CartProductCounter } from './components/cartProductCounter';
+export { CartProductInfo } from './components/cartProductInfo';
+export { CartProductSummary } from './components/cartProductSummary';
+export { CartTotalPrice } from './components/cartTotalPrice';
