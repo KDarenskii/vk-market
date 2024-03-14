0a588a50b79ef8609dcb0ddd86fb56cf35fccef1
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-14 23:00:46 +0300

    re-exported cart service

--- src/entities/cart/index.ts
+++ src/entities/cart/index.ts
@@ -4,3 +4,4 @@ export { CartProductSummary } from './components/cartProductSummary';
 export { CartTotalPrice } from './components/cartTotalPrice';
 export { cartReducer } from './store/cartStore';
 export type { CartItem } from './types/cartItem';
+export { CartService } from './services/CartService';
