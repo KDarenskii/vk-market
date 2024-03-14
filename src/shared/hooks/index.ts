0acfc603c61962d941016b1d1202f6343971d4a9
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-14 19:01:44 +0300

    removed exports

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,5 +1,2 @@
-export { useDebounceCallback } from './useDebounceCallback/useDebounceCallback';
-export type { UseDebounceCallbackOptions } from './useDebounceCallback/types';
-export { useLatest } from './useLatest';
 export { useTheme } from './useTheme';
 export { useSwitcherTheme } from './useSwitcherTheme';
