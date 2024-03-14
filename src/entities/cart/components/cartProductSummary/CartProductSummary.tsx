a2724e5ad283952a1cda6192c4427e93e78c6d24
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-14 18:59:24 +0300

    added cart product price summary component

---
+++ src/entities/cart/components/cartProductSummary/CartProductSummary.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+
+import { formatCurrency } from 'shared/helpers';
+
+import { Title } from '@vkontakte/vkui';
+
+import styles from './cartProductSummary.module.css';
+
+interface CartProductSummaryProps {
+  price: number;
+}
+
+export const CartProductSummary: FC<CartProductSummaryProps> = ({ price }) => {
+  return (
+    <div className={styles.productSummary}>
+      <Title weight="1">
+        Итого:{' '}
+        <span className={styles.price}>{formatCurrency(price)}</span>
+      </Title>
+    </div>
+  );
+};
---
+++ src/entities/cart/components/cartProductSummary/index.ts
@@ -0,0 +1,1 @@
+export { CartProductSummary } from './CartProductSummary';
