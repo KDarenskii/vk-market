ac8230386c2e74b0e82ae700d99cb44737c4dafb
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-14 21:07:12 +0300

    changed store import path

--- src/app/providers/WithRedux.tsx
+++ src/app/providers/WithRedux.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react';
 
 import { Provider } from 'react-redux';
 
-import { store } from 'shared/store';
+import { store } from 'store';
 
 export const WithRedux: FC<PropsWithChildren> = ({ children }) => {
   return <Provider store={store}>{children}</Provider>;
