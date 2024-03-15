43f05111e5767a1d34edc70d9a7c1223677adf39
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-15 11:03:36 +0300

    added handlers for fetch cart items thunk

--- src/entities/cart/store/cartStore.ts
+++ src/entities/cart/store/cartStore.ts
@@ -1,11 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 import { defaultCartState } from './defaultState';
+import { fetchCartItems } from './thunks/fetchCartItems';
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState: defaultCartState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCartItems.pending, (state) => {
+        state.error = null;
+        state.isLoading = true;
+        state.cartItems = [];
+      })
+      .addCase(fetchCartItems.fulfilled, (state, action) => {
+        state.error = null;
+        state.isLoading = false;
+        state.cartItems = action.payload;
+      })
+      .addCase(fetchCartItems.rejected, (state) => {
+        state.error = 'Что-то пошло не так';
+        state.isLoading = false;
+        state.cartItems = [];
+      });
+  },
 });
 
 export const cartReducer = cartSlice.reducer;
