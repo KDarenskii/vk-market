b9ed174168d3c8de79b667bd0fc3c03e15df5e62
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-15 09:57:47 +0300

    re-exported useDispatch and useSeletor hooks

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,2 +1,4 @@
 export { useTheme } from './useTheme';
 export { useSwitcherTheme } from './useSwitcherTheme';
+export { useAppDispatch } from './useAppDispatch';
+export { useAppSelector } from './useAppSelector';
