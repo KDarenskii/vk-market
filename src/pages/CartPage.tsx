13bcb741406a3fc41975219187fc2f00e869fef2
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-15 15:49:51 +0300

    added cart icon to panel header

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -2,13 +2,23 @@ import { FC } from 'react';
 
 import { Cart } from 'widgets/cart';
 
-import { Panel, PanelHeader, View } from '@vkontakte/vkui';
+import { IconButton, Panel, PanelHeader, View } from '@vkontakte/vkui';
+
+import { Icon28ShoppingCartOutline } from '@vkontakte/icons';
 
 export const CartPage: FC = () => {
   return (
     <View activePanel="cart">
       <Panel id="cart">
-        <PanelHeader>VK Market</PanelHeader>
+        <PanelHeader
+          after={
+            <IconButton onClick={() => {}}>
+              <Icon28ShoppingCartOutline />
+            </IconButton>
+          }
+        >
+          VK Market
+        </PanelHeader>
         <Cart />
       </Panel>
     </View>
