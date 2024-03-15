3c4c0c41d09b4aea43a1349a8c77d45747ef0f62
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-15 11:11:18 +0300

    fixed cart index.ts re-export

--- src/widgets/cart/index.ts
+++ src/widgets/cart/index.ts
@@ -1,3 +1,3 @@
-export { CartProduct } from './components/CartProduct';
+export { CartItem } from './components/CartItem';
 export { CartList } from './components/CartList';
 export { Cart } from './components/Cart';
