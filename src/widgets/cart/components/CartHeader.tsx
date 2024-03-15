9170480d25391b8457fb2ded7da5df8347df7eeb
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-15 11:09:23 +0300

    added cart items amount to cart header component

--- src/widgets/cart/components/CartHeader.tsx
+++ src/widgets/cart/components/CartHeader.tsx
@@ -2,15 +2,21 @@ import { FC } from 'react';
 
 import { Counter, Header } from '@vkontakte/vkui';
 
-export const CartHeader: FC = () => {
+interface CartHeaderProps {
+  cartItemsAmount: number;
+}
+
+export const CartHeader: FC<CartHeaderProps> = ({ cartItemsAmount }) => {
   return (
     <Header
       mode="primary"
       size="large"
       indicator={
-        <Counter size="m" mode="secondary">
-          3
-        </Counter>
+        !!cartItemsAmount && (
+          <Counter size="m" mode="secondary">
+            3
+          </Counter>
+        )
       }
     >
       Корзина товаров
