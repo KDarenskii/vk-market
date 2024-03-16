4f3bfb257d6556d3e64a65ea5508075272ba9ee7
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-16 19:25:03 +0300

    created useDebouce hook

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -2,3 +2,4 @@ export { useTheme } from './useTheme';
 export { useSwitcherTheme } from './useSwitcherTheme';
 export { useAppDispatch } from './useAppDispatch';
 export { useAppSelector } from './useAppSelector';
+export { useDebounce } from './useDebounce';
