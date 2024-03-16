fb1e24eb94f29a198cc38b202e886f887606ee53
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-16 13:12:06 +0300

    added fetch products handlers to extra reducers

--- src/entities/product/index.ts
+++ src/entities/product/index.ts
@@ -2,3 +2,6 @@ export type { Product } from './types/product';
 export { ProductCardRating } from './components/productCardRating';
 export { productsApi } from './api/productsApi';
 export { ProductsService } from './services/ProductsService';
+export { selectProductsState } from './store/selectors/selectProductsState';
+export { fetchProducts } from './store/thunks/fetchProducts';
+export { productsReducer } from './store/productsStore.ts';
--- src/entities/product/store/productsStore.ts
+++ src/entities/product/store/productsStore.ts
@@ -1,12 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { fetchProducts } from '..';
+
 import { defaultProductsState } from './defaultState';
 
 const productsSlice = createSlice({
   name: 'products',
   initialState: defaultProductsState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.error = null;
+        state.isLoading = true;
+        state.products = [];
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.error = null;
+        state.isLoading = false;
+        state.products = action.payload;
+      })
+      .addCase(fetchProducts.rejected, (state) => {
+        state.error = 'Что-то пошло не так';
+        state.isLoading = false;
+        state.products = [];
+      });
+  },
 });
 
-export const {} = productsSlice.actions;
 export const productsReducer = productsSlice.reducer;
