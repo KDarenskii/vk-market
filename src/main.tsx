7da27645fe742bdb14b711a8a139e1544121b378
Kirill Darenskii <kdarenskii@mail.ru>
2024-03-17 10:55:30 +0300

    removed ssl plugin and bridge initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 
-import bridge from '@vkontakte/vk-bridge';
 import ReactDOM from 'react-dom/client';
 
 import { App } from 'app/App';
-
-bridge.send('VKWebAppInit');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), basicSsl()],
+  plugins: [react(), tsconfigPaths()],
 });
